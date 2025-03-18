d0379d6c24adf16936824f2661a8bce945056f6e
zenerz <nehreh973@gmail.com>
2025-03-17 21:11:58 -0400

    new

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const { networkInterfaces } = require('os');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use('/src', express.static('src'));
+app.use('/bootstrap', express.static('node_modules/bootstrap/dist/css/bootstrap.min.css'));
+
+server.listen(PORT, () => {
+    console.log(`\nListening on ... \n`);
+    Object.values(networkInterfaces()).forEach( i => {
+        i.forEach( n => {
+            if (n.family !== 'IPv4') return;
+            console.log(`\t>\t\x1b[34mhttp://${n.address}:${PORT}`);
+        });
+    });
+    console.log('');
+})
+
+app.get('/', (req, res) => res.render('index') );
