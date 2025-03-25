a209f9504fca3415e663b932a45904af7d008530
Reh <Ne.N.Reh@baypath.net>
2025-03-25 14:13:44 -0400

    asd

--- build/fnaf2/officemovement.js
+++ build/fnaf2/officemovement.js
@@ -6,12 +6,12 @@ import root from "./rootcontainer";
 export default class OfficeMovement extends VisualAspect {
     static async init(root, parent) {
         super.init(root, parent);
-        const daAlpha = 0.0
+        const daAlpha = 0.25;
         this.movePercentage = 0.015;
         this.moveLeft, this.moveRight = false;
 
         /** @type {Graphics} */
-        this.leftRegion = this.add(new Graphics().rect(0, 0, root.nativeResolution.x*0.35, root.nativeResolution.y).fill(0xffff00));
+        this.leftRegion = this.add(new Graphics().rect(0, 0, root.nativeResolution.x*0.375, root.nativeResolution.y).fill(0xffff00));
         this.leftRegion.alpha = daAlpha;
         this.leftRegion.eventMode = 'static';
         this.leftRegion.onpointerenter = e => {
@@ -23,7 +23,7 @@ export default class OfficeMovement extends VisualAspect {
         }
 
         /** @type {Graphics} */
-        this.rightRegion = this.add(new Graphics().rect(root.nativeResolution.x*0.55, 0, root.nativeResolution.x*0.35, root.nativeResolution.y).fill(0x00ff00));
+        this.rightRegion = this.add(new Graphics().rect(root.nativeResolution.x*0.625, 0, root.nativeResolution.x*0.375, root.nativeResolution.y).fill(0x00ff00));
         this.rightRegion.alpha = daAlpha;
         this.rightRegion.eventMode = 'static';
         this.rightRegion.onpointerenter = e => {
